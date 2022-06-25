22fa3aed625c0ffbab54ab2989285a344fa45296
shizuka1997 <system.study.1997@gmail.com>
2022-06-25 13:07:06 +0900

    writer commit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,35 +1,43 @@
 @extends('layouts.layouts')
 
-@section('title', 'Simple Board')
+@section('title', 'ライター登録内容の編集')
 
 @section('content')
 
-<h1>Editing Post</h1>
+<h1>ライター登録内容の編集</h1>
+
 @if ($errors->any())
-    <div class="alert alert-danger" style="padding:10px;">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+     <div class="alert alert-danger">
+         <ul>
+             @foreach ($errors->all() as $error)
+                 <li>{{ $error }}</li>
+             @endforeach
+         </ul>
+     </div>
 @endif
 
-<form method="POST" action="/posts/{{ $post->id }}">
+<form action="/writer_update/{{ $writer->id }}" method="POST" >
     {{ csrf_field() }}
-    <input type="hidden" name="_method" value="PUT">
+
     <div class="form-group">
-        <label for="title">Title</label>
-        <input type="text" id="title" class="form-control" aria-describedby="emailHelp" name="title" value="{{old('title')}}">
+        <label for="w_1">ライター名</label>
+        <textarea class="form-control" name="w_n">{{ old('w_n', $writer->w_name) }}</textarea>
     </div>
+
     <div class="form-group">
-        <label for="content">Content</label>
-        <textarea id="content" class="form-control" name="content">{{old('content')}}</textarea>
+        <label for="w_2">住所</label>
+        <textarea class="form-control" name="w_a">{{ old('w_a', $writer->w_address) }}</textarea>
     </div>
-    <button type="submit" class="btn btn-outline-primary">Submit</button>
+
+    <div class="form-group">
+        <label for="w_3">電話番号</label>
+        <textarea class="form-control" name="w_t">{{ old('w_t', $writer->w_tel) }}</textarea>
+    </div>
+
+    <button type="submit" class="btn btn-outline-primary">編集</button>
 </form>
 
-<a href="/posts/{{ $post->id }}">Show</a> | 
-<a href="/posts">Back</a>
+<!-- <a href="/writer/{{ $writer->id }}">確認</a> |  -->
+<a href="/writer">戻る</a>
 
 @endsection
